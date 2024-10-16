83007279064c72c21b3c9fd9f0ccda11fdfe8f61
farhan <farhanvirenze18@gmail.com>
2024-10-16 21:13:45 +0700

    commit import ui color

--- app/src/main/java/com/pam/p2_basic_composs/MainActivity.kt
+++ app/src/main/java/com/pam/p2_basic_composs/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -77,6 +78,7 @@ fun BasicLayout(modifier: Modifier = Modifier) {
         // Name of the user (in red color)
         Text(
             text = "Muhamad Adri",
+            style = TextStyle(fontSize = 24.sp, color = Color.Red),
         )
     }
 }
